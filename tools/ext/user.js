614f7d414d5f5bf9247271c4597b6465ed1f4493
UDPF <36721880+udpf@users.noreply.github.com>
2020-05-18 22:10:44 +0530

    update user.js

--- tools/ext/user.js
+++ tools/ext/user.js
@@ -1,10 +1,11 @@
 
 //////////use of this//////////////////////
-
+/*
 javascript:var script = document.createElement('script'); script.src="//udpf.github.io/udpf/tools/ext/user.js"; document.head.appendChild(script);
+*/
 ///////////////////////////////////////////
 //// tool and library
-var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/ext/lib/tools.js "; document.head.appendChild(scriptl); 
+var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/ext/lib/tools.js "; document.head.appendChild(script1); 
 
  var script2 = document.createElement('script'); script2.src="//udpf.github.io/udpf/tools/ext/lib/filesaver.js "; document.head.appendChild(script2); 
  
