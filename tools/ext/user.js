1040003557f3a2aa3fe808d77021c5d242be3562
UDPF <36721880+udpf@users.noreply.github.com>
2020-05-18 21:44:04 +0530

    Update user.js

--- tools/ext/user.js
+++ tools/ext/user.js
@@ -1,2 +1,11 @@
+//// tool and library
+var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/ext/  "; document.head.appendChild(scriptl); 
 
-hgffvb
+ var script = document.createElement('script'); script.src="//udpf.github.io/udpf/tools/divtools/eruda  "; document.head.appendChild(script); 
+ 
+ 
+var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/divtools/eruda-code"; document.head.appendChild(script1); 
+
+var script2 = document.createElement('script'); script2.src="//udpf.github.io/udpf/tools/divtools/erudaint  "; document.head.appendChild(script2); 
+
+
