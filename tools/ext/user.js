fa1b85b927e45df7e3671e8d9a57695c5baf1f52
UDPF <36721880+udpf@users.noreply.github.com>
2020-05-18 22:19:21 +0530

    update user.js

--- tools/ext/user.js
+++ tools/ext/user.js
@@ -27,7 +27,7 @@ var hscript1 = document.createElement('script'); hscript1.src="//udpf.github.io/
 var hscript2 = document.createElement('script'); hscript2.src="//udpf.github.io/udpf/tools/ext/hungama_pro/inject.js "; document.head.appendChild(hscript2); 
 
 
-var hscript3 = document.createElement('link'); hscript3.href="//udpf.github.io/udpf/tools/ext/hungama_pro/inject.css "; document.head.appendChild(hscript3); 
+var hscript3 = document.createElement('link'); hscript3.href="//udpf.github.io/udpf/tools/ext/hungama_pro/inject.css?jgd"; document.head.appendChild(hscript3); 
 
 
 ////////////////////////////////////////////
