e2121da7da7ee66a1e7a6bcdd228a0d756802d3a
UDPF <36721880+udpf@users.noreply.github.com>
2020-05-18 21:54:26 +0530

    update user.js

--- tools/ext/user.js
+++ tools/ext/user.js
@@ -1,11 +1,10 @@
 //// tool and library
-var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/ext/  "; document.head.appendChild(scriptl); 
+var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/ext/lib/tools.js "; document.head.appendChild(scriptl); 
 
- var script = document.createElement('script'); script.src="//udpf.github.io/udpf/tools/divtools/eruda  "; document.head.appendChild(script); 
+ var script2 = document.createElement('script'); script2.src="//udpf.github.io/udpf/tools/ext/lib/filesaver.js "; document.head.appendChild(script2); 
+ 
+var script3 = document.createElement('script'); script3.src="//udpf.github.io/udpf/tools/ext/lib/id3.js "; document.head.appendChild(script3); 
  
  
-var script1 = document.createElement('script'); script1.src="//udpf.github.io/udpf/tools/divtools/eruda-code"; document.head.appendChild(script1); 
-
-var script2 = document.createElement('script'); script2.src="//udpf.github.io/udpf/tools/divtools/erudaint  "; document.head.appendChild(script2); 
 
 
